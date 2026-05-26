67e3d3d7b11f9ac56d28f2ff38dbb07f8fa27e71
Kevin Nørby Andersen <kevin@superultra.dk>
2026-05-26 17:20:23 +0200

    bold

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="flex h-full w-full flex-col items-center justify-center">
-	<div class="text-9xl text-blue-950">Nej</div>
+	<div class="text-9xl font-bold text-blue-950">Nej</div>
 </div>

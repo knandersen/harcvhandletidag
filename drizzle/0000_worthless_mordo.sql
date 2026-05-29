d77ab762749835e87637bf94004c5b5c9c14cc05
Kevin Nørby Andersen <kevin@superultra.dk>
2026-05-29 10:40:14 +0200

    migrations

--- drizzle/0000_worthless_mordo.sql
+++ drizzle/0000_worthless_mordo.sql
@@ -2,11 +2,3 @@ CREATE TABLE `hype` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`hypeLevel` integer
 );
---> statement-breakpoint
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`email` text,
-	`name` text
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,4 @@
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-
-export const users = sqliteTable('users', {
-	id: integer('id').primaryKey({ autoIncrement: true }),
-	email: text('email').unique(),
-	name: text('name')
-});
+import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
 
 export const hype = sqliteTable('hype', {
 	id: integer('id').primaryKey({ autoIncrement: true }),

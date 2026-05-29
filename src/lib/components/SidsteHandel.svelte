fa62aaf9552a343b853399ec3c01bfe4fb4af8c6
Kevin Nørby Andersen <kevin@superultra.dk>
2026-05-29 13:07:33 +0200

    string

--- src/lib/components/SidsteHandel.svelte
+++ src/lib/components/SidsteHandel.svelte
@@ -9,4 +9,4 @@
 	}, 1000);
 </script>
 
-Sidste handel var for<br /><span class="font-bold">{relativeTime}</span> siden.
+Sidste handel var for<br /><span class="font-bold">{relativeTime.string}</span> siden.
